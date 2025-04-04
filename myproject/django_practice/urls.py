f9ecf0b7cb99955e23dab232cd52771b30ad3169
JeevananthamI2I <jeevanantham@ideas2it.com>
2025-04-04 11:32:54 +0530

    i write code for django frmaework to practice

--- myproject/django_practice/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urls =[
-    path('', views.upload_file, name='upload_file'),  # List of authors
-    # path('author/<int:pk>/', views.author_list, name='author_detail'),  # Author detail page
-]
