631a83ada4437a095fe922fee901aa909d9a5654
JeevananthamI2I <jeevanantham@ideas2it.com>
2025-04-10 18:51:46 +0530

    work with ecomm app for crud operations

--- ecomm_app/urls.py
+++ ecomm_app/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('product/<int:pk>/', views.product_detail, name='product_detail'),  # Ensure this line exists
     path('product/add/', views.add_product, name='add_product'),
+    path('add-category/', views.add_category, name='add_category'),
+    path('', views.category_list, name='category_list'),
     path('product/edit/<int:pk>/', views.edit_product, name='edit_product'),
     path('product/delete/<int:pk>/', views.delete_product, name='delete_product'),
 ]
