4e20e9be61870566d776e5f25a1c20a29b28aeb2
JeevananthamI2I <jeevanantham@ideas2it.com>
2025-04-25 20:46:18 +0530

    soleved problems in hacker rank

--- hackerrank_problems.py
+++ hackerrank_problems.py
@@ -51,10 +51,8 @@ def find_runner_up_score():
     n = 5
     list1 = [2, 3 ,6, 6, 5]
     max_n= max(list1)
-
     filter=[i for i in list1 if i!=max_n]
     run_up=max(filter)
-
     print(run_up)
 find_runner_up_score()   
     
@@ -67,13 +65,92 @@ def count_substring(string, sub_string,count=0):
 count_substring("ABCDCDC","CDC")
 
 def string_validators():
-    str_list = list("qA2")
+    str_list = "qA2"
     methods = ['isalnum', 'isalpha', 'isdigit', 'islower', 'isupper']
-
+    q='q'.__getattribute__("isalnum")
+    print('q',bool(q))
     for method in methods:
         if any(getattr(char, method)() for char in str_list):
             print(True)
         else:
             print(False)
 string_validators()
+
+
+def marks():
+    n = 3
+    student_marks={"Krishna":[67, 68, 69],"Arjun":[70, 98, 63],"Malika":[52 ,56 ,60]}
+    st_marks = student_marks.get("Malika","key invalid")
+    avg_mark=0
+    count =0
+    for i in st_marks:
+         avg_mark +=i   
+         count +=1
+    re = format((avg_mark/count),".2f")
+    print (re)
+    
+marks()
+
+def tuples_hash():
+    # integer_list = map(int, input().split())
+    integer_list=[1,2,3,4]
+    value_tuple=tuple(integer_list)
+    print(hash(value_tuple))
+    
+tuples_hash()
+
+def list_prblm1():
+    N = int(input())
+    
+    
+    list1=[]
+    for _ in range(N):
+        command = input().split()
+        if command[0] == "insert":
+            i = int(command[1])
+            e = int(command[2])
+            list1.insert(i,e)
+        elif command[0] == "print":
+            print(list1)
+        elif command[0] == "remove":
+            e = command[1]
+            list1.remove(e)
+        elif command[0] == "append":
+            e =command[1]
+            list1.append(e)
+        elif command[0] =="sort":
+            list1.sort()
+        elif command[0] =="pop":
+            list1.pop()
+        elif command[0] =="reverse":
+            list1.reverse()
+        
+    
+# list_prblm1()
+
+def nested_list_p1():
+    names=[]
+    scores=[]
+    student=[]
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        scores.append(score)
+        names.append(name)
+        student.append([name,score])
+    value= list(set(scores))
+    min_v1=min(value)
+    list2=[i for i in scores if i!=min_v1]
+    print(student)
+    
+    # for i in student:
+    #     if i[1] == min(list2):
+    #         print(i[0])
+    print('\n'.join(sorted(i[0] for i in student if i[1] == min(list2))))
+
+    
+nested_list_p1()
+
+
+    
     
